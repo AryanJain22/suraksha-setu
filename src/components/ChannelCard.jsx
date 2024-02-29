e55c38ea7a6a765495756e5e7f9a695d0a931517
Aryan Jain <aryan.jain2021@vitbhopal.ac.in>
2024-02-29 10:27:47 +0530

    Added new photo for commity page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -6,7 +6,7 @@ const ChannelCard = ({ showChannels, setShowChannels }) => {
             <div className="flex justify-start items-center w-full p-2 hover:bg-[#212121cf] cursor-pointer gap-4 bg-[#212121] border-b border-black" onClick={() => setShowChannels(!showChannels)}>
                 <img
                     className="w-10 h-10 rounded-full inline"
-                    src="https://firebasestorage.googleapis.com/v0/b/infinity-blogs.appspot.com/o/profileImages%2FIron%20Man1668356649409?alt=media&token=441df548-91b5-44d7-ae5a-29fcb26e839c"
+                    src="https://lh3.googleusercontent.com/-GhZPGqbJsL4/WJCcYrBHHzI/AAAAAAAADFM/ZwfXrZUX73c/%25255BUNSET%25255D.jpg"
                     alt="Rounded avatar"
                 />
 
--- src/components/ChannelFeedArea.jsx
+++ src/components/ChannelFeedArea.jsx
@@ -25,7 +25,6 @@ const ChannelFeedArea = ({ showChannels, setShowChannels }) => {
                     <FeedCard />
                     <FeedCard />
                     <FeedCard />
-
                 </div>
             </div>
         </>
